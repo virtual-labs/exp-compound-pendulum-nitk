47554d722b0aed943a6662ebf95001476ff2a5ab
SOLVE-NITK <csd@nitk.edu.in>
2024-06-28 13:50:54 +0530

    Simulation Updated and pretest and postest explainations updated

--- experiment/procedure.md
+++ experiment/procedure.md
@@ -1,23 +1,22 @@
-1.	At first page, the animation of double pendulum is displayed as shown below:
-![](images/Procedure/1.png)
+### Procedure
 
-2.	The schematic representation of double pendulum is given as to denote the identity of each variable in variables toolbox.
+<div style="text-align:left">
+  Click on the Simulation icon <img src="images/simulation.png" alt="Alt text" style="height:80px; width:80px;">  to view the simulation. 
 
-3.	To change the values of the variables or to go to next page, click  ![](images/Procedure/2.png)  button.
+   ![Alt text](images/Simscreen1.png)
+   
+   In order to view the variable section click on the Variable icon. <img src="images/var1.png" alt="Alt text" style="height:80px; width:80px;">
 
-4.	To play the animation, click ![](images/Procedure/3.png)  button.
-![](images/Procedure/4.png)
+   ![Alt text](images/var2.png)
 
-5.	You can change the variables (l<sub>1</sub>, l<sub>2</sub>, θ<sub>1</sub>, θ<sub>2</sub>, m<sub>1</sub>, and m<sub>2</sub>) and conclude your inference according to your convenience.
+   Click on Play icon <img src="images/play1.png" alt="Alt text" style="height:80px; width:80px;"> to run the simulation and click on Pause icon <img src="images/pause.png" alt="Alt text" style="height:80px; width:80px;"> to stop it.
 
-6.	To go to next page, click ![](images/Procedure/5.png)  button.
+   Click on the Next icon  <img src="images/next2.png" alt="Alt text" style="height:80px; width:80px;"> to go further.  
 
-7.	At the second page, you will check the two natural frequencies of system and their respective mode shape ratios.
-![](images/Procedure/6.png)
+   ![Alt text](images/screen1.png)
 
-8.	At third page, you will check the mode shapes of the point masses with respect to their mode shape ratio and natural frequencies.
-![](images/Procedure/7.png)
+   Click on the Next or Prev icon<img src="images/prenex.png" alt="Alt text" style="height:80px; width:80px;"> to cycle through different pages. 
 
-9.	You can change the values of variables in any of the page only if you pause the animation.
+   ![Alt text](images/screen2.png)
 
-10.	To go back to the previous page, click  ![](images/Procedure/8.png)   button.
+</div>
--- experiment/simulation/js/sketch.js
+++ experiment/simulation/js/sketch.js
@@ -1,7 +1,8 @@
-﻿// canvas
-let width = 800;
+﻿  // canvas
+// let width = document.querySelector("#canvas-container").width;
+// let height = document.querySelector("#canvas-container").height;
+let width = 600;
 let height = 600;
-
 
 // counter
 let t = 0.1;
@@ -11,21 +12,19 @@ let dt = 0.01;
 let spring1;
 
 // graphs
-let position_graph1;
-let position_graph2;
+// let position_graph1;
+// let position_graph2;
 let magFac;
-let k1;
-let k2;
-let m1;
-let m2;
-let w;
-let F0;
+let magFac1;
+let magFac2;
+
+
 
 // factor
 let factor = 1;
 
 // images
-let img;
+// let img;
 let button1;
 let button2;
 let button3;
@@ -37,78 +36,266 @@ let spr;
 let page1 = true;
 let page2 = false;
 let page3 = false;
+// let graphStep = 0;
 
 // animation
 let animation = true;
 let touch = false;
-
+let simTimeId;
 // Buttons
 let clear;
 
+
 function preload() {
-    play = loadImage("images/blueplaydull.png");
-    pause = loadImage("images/bluepausedull.png");
-    graph = loadImage("images/bluefwddulls.png");
-    back = loadImage("images/bluebkdulls.png");
-    bg = loadImage("images/frame_copper_ver02.png");
-    spr = loadImage("images/spring.png");
-    dia= loadImage("images/dimg270.gif");
+   
+  spr = loadImage("images/spring.png");
+  dia= loadImage("images/dimg270.gif");
+  $('#playpausebutton').css({
+    "opacity":1,
+    "pointer-events":"auto"
+  });
+  $('#graphbutton').css({
+    "opacity":0.5,
+    "pointer-events":"none"
+  
+  });
 }
 
 function setup() {
-    textFont("Comic Sans MS");
-    createCanvas(width, height);
-    l1 = new NumberInput(620, 140, "l1(m)",0.050, 0.130,0.080 ,0.001,1, true); //(x, y, label, min, max, iniVal, change,fine_change, ifSlider)
-    l2= new NumberInput(620, 190, "l2(m)", 0.050, 0.130,0.080 ,0.001,1, true);
+  l1 = new NumberInput(620, 140, "l1(m)",0.050, 0.130,0.080 ,0.001,1, true); //(x, y, label, min, max, iniVal, change,fine_change, ifSlider)
+  l2= new NumberInput(620, 190, "l2(m)", 0.050, 0.130,0.080 ,0.001,1, true);
     a1 = new NumberInput(620, 240, "θ1 (degrees)",10,60 , 30, 1,1, true);
     m1 = new NumberInput(620, 290, "m1(kg)", 25, 60, 40, 1,1, true);
     a2 = new NumberInput(620, 340, "θ2 (degrees)", 10, 60 ,40, 1,1, true);
     m2 = new NumberInput(620, 380, "m2(kg)", 25, 60, 40, 1,1, true);
-    spring1 = new System(300, 150, 90, 25, a1.iniVal, a2.iniVal);
-    position_graph1 = new Graph(50, 295, 100, 220, "x1", "t");
+    spring1 = new System(300, 150, 90, 25, a1, a2);
+ 
+  var sketchCanvas = createCanvas(600, 400);
+  sketchCanvas.parent("canvas-container");
+
+  spring1 = new System(300, 150, 90, 25, a1, a2);
+
+  position_graph1 = new Graph(50, 295, 100, 220, "x1", "t");
     position_graph2 = new Graph(50, 210, 100, 220, "x2", "t");
     magFac1 = new DynamicGraph(50, 400, 300, 220, "X1/Xst", "ω/ω2", 0,7,0,10, System.mag_func1);
     magFac2 = new DynamicGraph(50, 400, 300, 220, "X2/Xst", "ω/ω2",0, 7,0,10, System.mag_func2);
-    button1 = new Button(645, 430, pause)
-    button2 = new Button(706, 430, graph)
-    button3 = new Button(645,460,back)
-    button4 = new Button(705, 460, graph)
-    button5 = new Button(645,470,back)
+
+    varinit();
+    l1 = $("#fSpinner").spinner("value");
+    l2 = $("#omegaSpinner").spinner("value");
+    a1 = $("#k1Spinner").spinner("value");
+    m1 = $("#m1Spinner").spinner("value");
+    a2 = $("#k2Spinner").spinner("value");
+    m2 = $("#m2Spinner").spinner("value");
+    spring1 = new System(300, 150, 90, 25, a1, a2);
+
+  magFac1 = new DynamicGraph(50,
+    400,
+    300,
+    220,
+    "X1/Xst",
+    "ω/ω2",
+    0,
+    7,
+    0,
+    10,
+    System.mag_func1
+  );
+  magFac2 = new DynamicGraph(
+    50,
+    400,
+    300,
+    220,
+    "X2/Xst",
+    "ω/ω2",
+    0,
+    7,
+    0,
+    10,
+    System.mag_func2
+  );
+  
+   
 }
+
 function draw() {
-    if (page1) {
-        runPage1();
-         if(animation==true){
-        l1.disable();
-        l2.disable();
-        a1.disable();
-        m1.disable();
-        a2.disable();
-        m2.disable();
-    }
-     else{
-        l1.enable();
-            l2.enable();
-            a1.enable();
-            m1.enable();
-            a2.enable();
-            m2.enable();
-            spring1.a1v=0;
-            spring1.a2v=0;
-            spring1.a1a=0.01;
-            spring1.a2a=0.01;
-            t=0.1;
-            spring1 = new System(300, 150, 90, 25, a1.inp, a2.inp);     
-     }
-    }
-    if (page2) {
-        runPage2();
-    }
-    if (page3){
-        runPage3();
-    }
+  if (page1) {
+    runPage1();
+    if(animation==true){
+   
+  }
+  else{
+    
+        spring1.a1v=0;
+        spring1.a2v=0;
+        spring1.a1a=0.01;
+        spring1.a2a=0.01;
+        t=0.1;
+        spring1 = new System(300, 150, 90, 25, a1, a2);     
+ }
+  }
+
+  if (page2) {
+    runPage2();
+  }
+
+  if (page3) {
+    runPage3();
+  }
+}
+function simstate()
+{
+  var imgfilename=document.getElementById('playpausebutton').src;
+  imgfilename = imgfilename.substring(imgfilename.lastIndexOf('/') + 1, imgfilename.lastIndexOf('.'));
+  if (imgfilename=="bluepausedull")
+  {
+    animation = false;
+    document.getElementById('playpausebutton').src="images/blueplaydull.svg";
+	 clearInterval(simTimeId);
+    simstatus=0;
+    // $('#theta2spinner').spinner("value",theta2);			//to set simulation parameters on pause
+    pauseTime=setInterval("varupdate();",'100');
+    document.querySelector(".playPause").textContent = "Play";
+    $('.variables-sections').css({
+          "opacity":1,
+          "pointer-events":"auto"
+        });
+        $('#graphbutton').css({
+          "opacity":1,
+          "pointer-events":"auto"
+        
+        });
+  }
+    if (imgfilename=="blueplaydull")
+  {
+    animation = true;
+  	 time=0;			
+  	 clearInterval(pauseTime);
+    document.getElementById('playpausebutton').src="images/bluepausedull.svg";
+    simTimeId=setInterval("time=time+0.1; varupdate(); ",'100');    
+    simstatus=1;
+    document.querySelector(".playPause").textContent = "Pause";
+    $('.variables-sections').css({
+        "opacity":0.5,
+        "pointer-events":"none"
+      });
+      $('#graphbutton').css({
+        "opacity":0.5,
+        "pointer-events":"none"
+      });
+  } 
+}
+
+
+function graphPlot() {
+  graphStep = 1;
+  document.querySelector(".graph-one").classList.remove("display-hide");
+  document.querySelector(".graph-two").classList.remove("display-hide");
+  document.querySelector(".graph-div span").textContent = "Prev/Next";
+  document.querySelector(".graph-button").style.display = "none";
+  screenchangePhase();
+  page1=false;
+  page2=true;
+  page3=false;
+  runPage2();
+  console.log("nextN")
+  $('#playpausebutton').css({
+    "opacity":0.5,
+    "pointer-events":"none"
+  });
+
+}
+
+function screenchangePhase() {
+ 
+  phaseAngleGraph();
+  
+}
+
+function screenchangeMag() {
+  magnitudeGraph();
+  // page1=false;
+  // page2=false;
+  // page3=true;
+  // runPage3();
+  graphStep += 1;
+ 
+  document.querySelector(".graph-two").classList.add("display-hide");
+  document.querySelector(".graph-div span").textContent = "Prev";
+
+}
+
+function screenChangePrevious() {
+  graphStep -= 1;
+  if (graphStep > 0) {
+    phaseAngleGraph();
+    $('#playpausebutton').css({
+      "opacity":0.5,
+      "pointer-events":"none"
+    });
+    document.querySelector(".graph-two").classList.remove("display-hide");
+    document.querySelector(".graph-div span").textContent = "Prev/Next";
+    console.log("a");
+    // page1 = true;
+    // page2 = false;
+    // page3 = false;
+    // runPage1();
+  } 
+  
+  else {
+    document.querySelector(".graph-div span").textContent = "";
+    document.querySelector(".graph-button").style.display = "flex";
+    document.querySelector(".graph-one").classList.add("display-hide");
+    document.querySelector(".graph-two").classList.add("display-hide"); 
+    console.log("b");
+     $('#playpausebutton').css({
+      "opacity":1,
+      "pointer-events":"auto"
+    });
+
+    page1 = true;
+    page2 = false;
+    page3 = false;
+    runPage1();
+    // console.log("page2")
+
+
+    document.querySelector(".graph-zero").classList.remove("display-hide");
+    document.querySelector(".graph-button span").textContent = "Next";
+    //  document.querySelector(".graph-button").classList.remove("display-hide");
+    document.querySelector(".graph-div").classList.add("display-hide");
+  }
+}
+
+function phaseAngleGraph() {
+  // resetGraphs();
+  page1 = false;
+  page2 = true;
+  page3 = false;
+  runPage2();
+  $('#playpausebutton').css({
+    "opacity":0.5,
+    "pointer-events":"none"
+  });
+
+  magFac1.initialise();
+  // phaseAng.initialise();
+}
+
+function magnitudeGraph() {
+  // resetGraphs();
+  page1 = false;
+  page2 = false;
+  page3 = true;
+  runPage3();
+  $('#playpausebutton').css({
+    "opacity":0.5,
+    "pointer-events":"none"
+  });
+console.log("SECOND")
+  magFac2.initialise();
+  // phaseAng.initialise();
 }
 function mousePressed() {
-    console.log(mouseX, mouseY)
-    handleEvents();
+ 
 }
